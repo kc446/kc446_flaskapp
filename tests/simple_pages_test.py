77e6975d7cac3aa9371ac19976336bf846d39a50
kc446 <kc446@njit.edu>
2022-03-06 22:14:39 -0500

    ah. that's why the pytest failed. okay.

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,8 +1,8 @@
-"""This test the homepage"""
+"""This tests the homepage"""
 
 
 def test_request_main_menu_links(client):
-    """This makes the index page"""
+    """This makes the navigation links"""
     response = client.get("/")
     assert response.status_code == 200
     assert b'<li><a href="/page/about">About</a></li>' in response.data
@@ -10,21 +10,21 @@ def test_request_main_menu_links(client):
 
 
 def test_request_about(client):
-    """This makes the index page"""
+    """This makes the about page"""
     response = client.get("/page/about")
     assert response.status_code == 200
     assert b"About Page" in response.data
 
 
 def test_request_welcome(client):
-    """This makes the index page"""
+    """This makes the welcome page"""
     response = client.get("/page/welcome")
     assert response.status_code == 200
     assert b"Welcome Page" in response.data
 
 
 def test_request_page_not_found(client):
-    """This makes the index page"""
+    """This makes the 404 page"""
     response = client.get("/page/page5")
     assert response.status_code == 404
 
