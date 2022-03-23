2f149825b7cf6ae289ee36dc6aaf03b9a0415f47
kc446 <stripedsweater1!>
2022-03-23 14:01:14 -0400

    starting project #2 and 'merging' the appropriate repository into this one

--- flaskApp/simple_pages/__init__.py
+++ flaskApp/simple_pages/__init__.py
@@ -1,15 +1,27 @@
-from flask import Blueprint, render_template, abort, Flask
+from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
+simple_pages = Blueprint('simple_pages', __name__,
+                        template_folder='templates')
 
-bp = Blueprint('simple_pages', __name__,
-                        template_folder='templates',static_folder='static')
 
-@bp.route('/page/<page>')
-def show(page):
+@simple_pages.route('/')
+def index():
     try:
-        return render_template('%s.html' % page)
+        return render_template('index.html')
     except TemplateNotFound:
         abort(404)
 
+@simple_pages.route('/about')
+def about():
+    try:
+        return render_template('about.html')
+    except TemplateNotFound:
+        abort(404)
 
+@simple_pages.route('/welcome')
+def welcome():
+    try:
+        return render_template('welcome.html')
+    except TemplateNotFound:
+        abort(404)
