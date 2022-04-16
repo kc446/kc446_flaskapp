367203e643e88d6b84bff965f3a6b933d0df3cb1
kc446 <kc446@njit.edu>
2022-04-16 01:16:35 -0400

    is test-config considered an argument? is that why heroku keeps fighting me??

--- flaskApp/__init__.py
+++ flaskApp/__init__.py
@@ -22,7 +22,7 @@ login_manager = LoginManager()
 def page_not_found(e):
     return render_template("404.html"), 404
 
-def create_app(test_config=None):
+def create_app(): #(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
 
@@ -42,12 +42,12 @@ def create_app(test_config=None):
         DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
-    if test_config is None:
+    #if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
+        #app.config.from_pyfile("config.py", silent=True)
+    #else:
         # load the test config if passed in
-        app.config.update(test_config)
+        #app.config.update(test_config)
         # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
@@ -79,7 +79,6 @@ def create_app(test_config=None):
 
     db.init_app(app)
     app.cli.add_command(create_database)
-
 
     return app
 
