11b0f0a348e3f8131dcd9df7f6e25872e5d61175
kc446 <kc446@njit.edu>
2022-04-20 14:12:20 -0400

    adding necessary files/directories for next project - doing this before i start rearranging the config stuff

--- flaskApp/db/models/__init__.py
+++ flaskApp/db/models/__init__.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
-from flask_login import UserMixin
-from flask_login._compat import unicode
+from sqlalchemy.orm import relationship
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskApp.db import db
+from flask_login import UserMixin
+from flask_login._compat import unicode
 
 
 class User(UserMixin, db.Model):
@@ -56,3 +57,32 @@ class User(UserMixin, db.Model):
     #     id = db.Column(db.Integer(), primary_key=True)
     #     user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
     #     role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
+
+class Location():
+    __tablename__ = 'locations'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=True, unique=False)
+    longitude = db.Column(db.String(300), nullable=True, unique=False)
+    latitude = db.Column(db.String(300), nullable=True, unique=False)
+    population = db.Column(db.Integer, nullable=True, unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = relationship("User", back_populates="locations", uselist=False)
+
+    def __init__(self, title, longitude, latitude, population):
+        self.title = title
+        self.longitude = longitude
+        self.latitude = latitude
+        self.population = population
+
+class Song(db.Model):
+    __tablename__ = 'songs'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=True, unique=False)
+    artist = db.Column(db.String(300), nullable=True, unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = relationship("User", back_populates="songs", uselist=False)
+
+    def __init__(self, title, artist):
+        self.title = title
+        self.artist = artist
+
